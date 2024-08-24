bd64bb41ff81c9e1a7fd72670afa60e5161a8475
gauravkumar222 <49018413+gauravkumar222@users.noreply.github.com>
2024-08-24 10:10:33 +0530

    Update LimitOrderAgentTest.java
    Test cases

--- limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
+++ limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
@@ -2,11 +2,43 @@ package org.afob.limit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.afob.execution.ExecutionClient;
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.times;
 
 public class LimitOrderAgentTest {
 
-    @Test
-    public void addTestsHere() {
-        Assert.fail("not implemented");
+    private ExecutionClient executionClient;
+    private LimitOrderAgent limitOrderAgent;
+
+    @Before
+    void initializeTest() {
+        executionClient = Mockito.mock(ExecutionClient.class);
+        limitOrderAgent = new LimitOrderAgent(executionClient);
     }
+
+    @Test
+    void testIBMOrderExecutedWhenPriceBelow100() throws ExecutionClient.ExecutionException {
+        limitOrderAgent.addOrder(true, "IBM", 1000, new BigDecimal("100"));
+        // Price tick that should trigger execution
+        limitOrderAgent.priceTick("IBM", 95.50);
+
+        // Verify that the order was executed
+        Mockito.verify(executionClient).buy(any());
+    }
+
+    @Test
+    void testIBMOrderNotExecutedWhenPriceAbove100() throws ExecutionClient.ExecutionException {
+        limitOrderAgent.addOrder(true, "IBM", 1000, new BigDecimal("100"));
+        // Price tick that should not trigger execution
+        limitOrderAgent.priceTick("IBM", 101.50);
+
+        // Verify that the order was not executed
+        Mockito.verify(executionClient).buy(any());
+    }
+
 }
